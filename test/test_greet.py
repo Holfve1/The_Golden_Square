a294bcb057ddbab36923aeb2a56efa49274ecc9f
christopher <chris.holfve@hotmail.com>
2025-06-09 14:26:57 +0100

    python files Greet & practice

---
+++ test/test_greet.py
@@ -0,0 +1,6 @@
+from lib.greet import *
+
+def greet_Hello_John():
+    result = "Hello, John!"
+
+    assert result == "Hello, John!"
---
+++ test/test_practice_testing.py
@@ -0,0 +1,5 @@
+from lib.practice_testing import *
+def test_adding_5_to_number():
+    result = add_five(3)
+
+    assert result == 8
